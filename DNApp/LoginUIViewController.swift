b5299ffc0eaf95bdfc1b369c08f98cfbacdb1a03
Jorge Luiz <jorge.luizchaves@yahoo.com.br>
2017-04-14 19:53:52 -0300

    session swiftapp-connecting done

---
+++ DNApp/LoginUIViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginUIViewController.swift
+//  DNApp
+//
+//  Created by Jorge Luiz on 14/04/17.
+//  Copyright © 2017 Jorge Luiz. All rights reserved.
+//
+
+import UIKit
+import Spring
+
+class LoginUIViewController: UIViewController {
+
+    @IBOutlet weak var dialogView: DesignableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginButtonDidTouch(_ sender: DesignableButton) {
+        
+        self.errorLogin()
+        
+    }
+    
+    private func errorLogin() {
+        dialogView.animation = "shake"
+        dialogView.animate()
+    }
+}
