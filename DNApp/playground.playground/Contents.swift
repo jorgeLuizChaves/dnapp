dc8e6d9f35de21a01919e23a34b331956113a08c
Jorge Luiz <jorge.luizchaves@yahoo.com.br>
2017-04-14 20:33:52 -0300

    finished text part

---
+++ DNApp/playground.playground/Contents.swift
@@ -0,0 +1,103 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// creating a variable
+var number = 4
+var number2 = 3
+
+print(number + number2)
+
+// constant
+let name = "Jonh"
+
+/*
+ It's not necessary to declare the variable type, but you can do that
+ */
+
+let surName: String = "Connor"
+
+//Arrays
+
+var colors = ["red", "blue", "green"]
+
+//add in an Array
+
+colors.append("yellow")
+
+
+//remove 
+
+colors.remove(at: 0)
+
+print(colors)
+
+//for loop
+
+for color in colors {
+    print(color)
+}
+
+//functions
+
+func pointsToRetina(point: Int) -> Int {
+    return point * 2
+}
+
+pointsToRetina(point: 3)
+
+
+//Class and Struct
+
+//Struct has a default Initializer, Struct is a value type
+//https://developer.apple.com/swift/blog/?id=10
+struct UserStruct {
+    var name: String
+    var age: Int
+    var job: String
+}
+
+var user = UserStruct(name: "Jorge", age: 30, job: "Consultant")
+
+user.name
+
+//Class its necessary to declare a init function and its a reference type
+//https://developer.apple.com/swift/blog/?id=10
+
+class User{
+    var name:String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+var jonh = User.init(name: "Jonh")
+
+print(jonh.name)
+
+
+//Optionals
+
+var numberX:Int? = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
