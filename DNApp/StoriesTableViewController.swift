4b15dcd6b26cf799bf5d51169602d5b34bb120fd
Jorge Luiz <jorge.luizchaves@yahoo.com.br>
2017-04-25 11:13:05 -0300

    finished class tableView Class and Content

---
+++ DNApp/StoriesTableViewController.swift
@@ -0,0 +1,69 @@
+//
+//  StoriesTableViewController.swift
+//  DNApp
+//
+//  Created by Jorge Luiz on 25/04/17.
+//  Copyright © 2017 Jorge Luiz. All rights reserved.
+//
+
+import UIKit
+
+class StoriesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.estimatedRowHeight = 100
+        tableView.rowHeight = UITableViewAutomaticDimension
+        // Do any additional setup after loading the view.
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Table View Functions
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StoryCell") as UITableViewCell!
+        
+        return cell!
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        performSegue(withIdentifier: "WebSegue", sender: self)
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    
+    // MARK: Events Touches
+    
+    @IBAction func loginButtonDidTouch(_ sender: Any) {
+        performSegue(withIdentifier: "loginSegue", sender: self)
+    }
+
+    @IBAction func menuButtonDidTouch(_ sender: Any) {
+        
+        performSegue(withIdentifier: "menuSegue", sender: self)
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DNApp/WebViewController.swift
@@ -0,0 +1,46 @@
+//
+//  WebViewController.swift
+//  DNApp
+//
+//  Created by Jorge Luiz on 25/04/17.
+//  Copyright © 2017 Jorge Luiz. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let targetURL = URL(string: "https://designcode.io/")
+        let request = URLRequest(url: targetURL!)
+        webView.loadRequest(request)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func closeButtonDidTouch(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
