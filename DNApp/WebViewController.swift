a2750b89e9b559b7255f7cbfa5e4eba1b5494b57
Jorge Luiz <jorge.luizchaves@yahoo.com.br>
2017-04-30 11:44:32 -0300

    swiftapp-openeninglinks done

--- DNApp/WebViewController.swift
+++ DNApp/WebViewController.swift
@@ -7,21 +7,79 @@
 //
 
 import UIKit
+import Spring
 
-class WebViewController: UIViewController {
+class WebViewController: UIViewController, UIWebViewDelegate {
 
     @IBOutlet weak var webView: UIWebView!
+    var url: String!
+    var hasFinishedLoading = false
     
+    let oneSecond = 1.0
+    let isToRepeat = true
+
+    
+    @IBOutlet weak var progressView: UIProgressView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        webView.delegate = self
+        let targetURL = URL(string: url)
+        let request = URLRequest(url: targetURL!)
+        webView.loadRequest(request)
+        
         // Do any additional setup after loading the view.
     }
     
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        hasFinishedLoading = false
+        updateProgress()
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView){
+        delay(delay: oneSecond) { [weak self] in
+            if let _self = self {
+                _self.hasFinishedLoading = true
+            }
+        }
+    }
+    
+    deinit {
+        webView.stopLoading()
+        webView.delegate = nil
+    }
+    
+    func updateProgress() {
+        if progressView.progress >= 1 {
+            progressView.isHidden = true
+        } else {
+            
+            if hasFinishedLoading {
+                progressView.progress += 0.002
+            } else {
+                if progressView.progress <= 0.3 {
+                    progressView.progress += 0.004
+                } else if progressView.progress <= 0.6 {
+                    progressView.progress += 0.002
+                } else if progressView.progress <= 0.9 {
+                    progressView.progress += 0.001
+                } else if progressView.progress <= 0.94 {
+                    progressView.progress += 0.0001
+                } else {
+                    progressView.progress = 0.9401
+                }
+            }
+            
+            delay(delay: 0.008) { [weak self] in
+                if let _self = self {
+                    _self.updateProgress()
+                }
+            }
+    
+        }
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
-        let targetURL = URL(string: "https://designcode.io/")
-        let request = URLRequest(url: targetURL!)
-        webView.loadRequest(request)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,6 +88,7 @@ class WebViewController: UIViewController {
     }
     
     @IBAction func closeButtonDidTouch(_ sender: Any) {
+        UIApplication.shared.isStatusBarHidden = false
         dismiss(animated: true, completion: nil)
     }
 
